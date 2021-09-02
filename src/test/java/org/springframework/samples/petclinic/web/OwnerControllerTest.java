4344d4cc773d376385f213dbc01caf7abf83637e
Igor A Fraga <igoravancinifraga@gmail.com>
2021-09-01 22:18:56 -0300

    163 and 164 - Assignment test GET method with Spring MVC Test and Using Spring MVC Test with Form Parameters - Spring MVC Test

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,9 +1,15 @@
 package org.springframework.samples.petclinic.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
@@ -14,8 +20,26 @@ class OwnerControllerTest {
     @Autowired
     ClinicService clinicService;
 
-    @Test
-    void test() {
+    MockMvc mockMvc;
 
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void initCreationFormTest() throws Exception {
+        mockMvc.perform(get("/owners/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"));
+    }
+
+    @Test
+    void testFindByNameNotFound() throws Exception {
+        mockMvc.perform(get("/owners")
+                        .param("lastName", "Dont find me!")) //this value will be injected into lastName attribute in the Owner object argument from processFindForm
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
     }
 }
